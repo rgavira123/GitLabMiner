44a6ee74b4f31f7bf7085938ce62affa460389d3
rgavira123 <ramgavsan@alum.us.es>
2023-04-30 17:10:29 +0200

    Initial commit

---
+++ src/main/java/aiss/gitlabminer/GitlabminerApplication.java
@@ -0,0 +1,13 @@
+package aiss.gitlabminer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabminerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitlabminerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aiss/gitlabminer/GitlabminerApplicationTests.java
@@ -0,0 +1,13 @@
+package aiss.gitlabminer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitlabminerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
