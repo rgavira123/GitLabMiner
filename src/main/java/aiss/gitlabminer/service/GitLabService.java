ed0510bf653370c10db67a8ad7f2fe9b4b58a166
rgavira123 <ramgavsan@alum.us.es>
2023-05-01 19:01:02 +0200

    GitLabMiner Service update and Pagination
    Co-authored-by: albcarsic <albcarsic@users.noreply.github.com>
    Co-authored-by: Rafa <rafpulcif@users.noreply.github.com>
    Co-authored-by: lucianoya <lucianoya@users.noreply.github.com>

--- src/main/java/aiss/gitlabminer/service/GitLabService.java
+++ src/main/java/aiss/gitlabminer/service/GitLabService.java
@@ -4,15 +4,21 @@ package aiss.gitlabminer.service;
 import aiss.gitlabminer.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import aiss.gitlabminer.util.GitLabUtils;
+
 
 import java.time.LocalDateTime;
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 @Service
 public class GitLabService {
@@ -26,11 +32,18 @@ public class GitLabService {
     public ResponseEntity<Project> getProjectById(String id){
         String uri = "https://gitlab.com/api/v4/projects/"+id;
 
-        ResponseEntity<Project> response = restTemplate.exchange(uri, HttpMethod.GET, null, Project.class);
-        return response;
+        return restTemplate.exchange(uri, HttpMethod.GET, null, Project.class);
     }
 
-    public ResponseEntity<Commit[]> getAllCommits(String id, Integer since) {
+    public ResponseEntity<Commit[]> getAllCommits(String id) {
+        String uri = "https://gitlab.com/api/v4/projects/" + id + "/repository/commits/";
+
+        return getResponseEntity(uri,Commit[].class);
+    }
+
+    public List<Commit> groupAllCommits(String id, Integer since, Integer maxPages) {
+        List<Commit> commits = new ArrayList<>();
+        Integer defaultPages;
         String uri = "https://gitlab.com/api/v4/projects/" + id + "/repository/commits/";
 
         if (since != null) {
@@ -39,31 +52,125 @@ public class GitLabService {
             uri += "?since=" + LocalDateTime.now().minusDays(2);
         }
 
-        ResponseEntity<Commit[]> response = restTemplate.exchange(uri, HttpMethod.GET, null, Commit[].class);
-        return response;
+        ResponseEntity<Commit[]> response = getResponseEntity(uri, Commit[].class);
+        List<Commit> pageCommits = Arrays.stream(response.getBody()).toList();
+        commits.addAll(pageCommits);
+
+        //2..n pages
+        String nextPageURL = GitLabUtils.getNextPageUrl(response.getHeaders());
+
+        if(maxPages!=null){
+            defaultPages=maxPages;
+        }
+        else{
+            defaultPages=2;
+        }
+
+        int page = 2;
+        while (nextPageURL != null && page <= defaultPages) {
+            response = getResponseEntity(nextPageURL,Commit[].class);
+            pageCommits = Arrays.stream(response.getBody()).toList();
+            commits.addAll(pageCommits);
+
+            nextPageURL = GitLabUtils.getNextPageUrl(response.getHeaders());
+            page++;
+
+        }
+        return commits;
+
     }
 
-    public ResponseEntity<Issue[]> getAllIssues(String id, Integer updated_after){
+    public ResponseEntity<Issue[]> getAllIssues(String id){
+        String uri = "https://gitlab.com/api/v4/projects/" + id + "/issues";
+
+        return getResponseEntity(uri,Issue[].class);
+    }
+
+    public List<Issue> groupAllIssues(String id, Integer updated_after, Integer maxPages) {
+        List<Issue> issues = new ArrayList<>();
+        Integer defaultPages;
         String uri = "https://gitlab.com/api/v4/projects/" + id + "/issues";
 
         if (updated_after != null) {
-            uri += "?updated_after=" + LocalDateTime.now().minusDays(updated_after);
+            uri += "/?updated_after=" + LocalDateTime.now().minusDays(updated_after);
         } else {
-            uri += "?updated_after=" + LocalDateTime.now().minusDays(20);
+            uri += "/?updated_after=" + LocalDateTime.now().minusDays(20);
         }
 
-        ResponseEntity<Issue[]> response = restTemplate.exchange(uri, HttpMethod.GET, null, Issue[].class);
-        return response;
+        ResponseEntity<Issue[]> response = getResponseEntity(uri,Issue[].class);
+        List<Issue> pageIssues = Arrays.stream(response.getBody()).toList();
+        issues.addAll(pageIssues);
+
+        //2..n pages
+        String nextPageURL = GitLabUtils.getNextPageUrl(response.getHeaders());
+
+        if(maxPages!=null){
+            defaultPages=maxPages;
+        }
+        else{
+            defaultPages=2;
+        }
+
+        int page = 2;
+        while (nextPageURL != null && page <= defaultPages) {
+            response = getResponseEntity(nextPageURL,Issue[].class);
+            pageIssues = Arrays.stream(response.getBody()).toList();
+            issues.addAll(pageIssues);
+            nextPageURL = GitLabUtils.getNextPageUrl(response.getHeaders());
+            page++;
+        }
+        return issues;
     }
 
     public ResponseEntity<Comment[]> getIssueComments(String id, String issue_iid){
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("PRIVATE-TOKEN", token);
-        HttpEntity<Comment[]> request = new HttpEntity<>(null,headers);
 
         String uri = "https://gitlab.com/api/v4/projects/" + id + "/issues/" + issue_iid + "/notes";
 
-        ResponseEntity<Comment[]> response = restTemplate.exchange(uri, HttpMethod.GET, request, Comment[].class);
-        return response;
+        return getResponseEntity(uri,Comment[].class);
     }
+
+    public List<Comment> groupIssueComments(String id, String issue_iid, Integer maxPages){
+        List<Comment> comments = new ArrayList<>();
+        Integer defaultPages;
+        String uri = "https://gitlab.com/api/v4/projects/" + id + "/issues/" + issue_iid + "/notes";
+
+        ResponseEntity<Comment[]> response = getResponseEntity(uri, Comment[].class);
+        List<Comment> pageComments = Arrays.stream(response.getBody()).toList();
+        comments.addAll(pageComments);
+
+        //2..n pages
+        String nextPageURL = GitLabUtils.getNextPageUrl(response.getHeaders());
+
+        if(maxPages!=null){
+            defaultPages=maxPages;
+        }
+        else{
+            defaultPages=2;
+        }
+
+        int page = 2;
+        while(nextPageURL != null && page <= defaultPages){
+            response = getResponseEntity(nextPageURL,Comment[].class);
+            pageComments = Arrays.stream(response.getBody()).toList();
+            comments.addAll(pageComments);
+
+            nextPageURL = GitLabUtils.getNextPageUrl(response.getHeaders());
+            page++;
+        }
+
+        return comments;
+    }
+
+    private <T1> ResponseEntity<T1[]> getResponseEntity(String uri, Class<T1[]> clase) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("PRIVATE-TOKEN", token);
+
+        HttpEntity<T1[]> request = new HttpEntity<>(null,headers);
+
+        return restTemplate.exchange(uri,
+                HttpMethod.GET,
+                request,
+                clase);
+    }
+
 }
--- src/test/java/aiss/gitlabminer/service/GitLabServiceTest.java
+++ src/test/java/aiss/gitlabminer/service/GitLabServiceTest.java
@@ -10,38 +10,66 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Arrays;
-
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class GitLabServiceTest {
 
     @Autowired
     GitLabService gitLabService;
+
     @Test
     void getProjectById() {
         ResponseEntity<Project> project = gitLabService.getProjectById("4207231");
-        assertTrue(project.getBody()!=null, "the project is empty");
+        assertTrue(project.getBody() != null, "the project is empty");
         System.out.println(project.getBody());
     }
 
     @Test
     void getAllCommits() {
-        ResponseEntity<Commit[]> commits = gitLabService.getAllCommits("4207231",null);
-        assertTrue(commits.getBody()!=null, "no commits have been found");
+        ResponseEntity<Commit[]> commits = gitLabService.getAllCommits("4207231");
+        assertTrue(commits.getBody() != null, "no commits have been found");
         System.out.println(Arrays.stream(commits.getBody()).toList());
     }
 
     @Test
     void getAllIssues() {
-        ResponseEntity<Issue[]> issues = gitLabService.getAllIssues("4207231",null);
-        assertTrue(issues.getBody()!=null,"no issues have been found");
+        ResponseEntity<Issue[]> issues = gitLabService.getAllIssues("4207231");
+        assertTrue(issues.getBody() != null, "no issues have been found");
         System.out.println(Arrays.stream(issues.getBody()).toList());
     }
-    
+
+    //funcion modificada, para que devuelva ya una lista, ya tenemos el metodo que lo hace, el que se llama group.
     @Test
     void getIssueComments() {
         ResponseEntity<Comment[]> comments = gitLabService.getIssueComments("4207231", "1234");
-        assertTrue(comments.getBody()!=null, "no comments were found");
+        assertTrue(comments.getBody() != null, "no comments were found");
         System.out.println(Arrays.stream(comments.getBody()).toList());
     }
+
+    @Test
+    void groupAllCommits() {
+        List<Commit> place = gitLabService.groupAllCommits("4207231", null, 2);
+        assertTrue(place.size() != 0, "the list of commits is empty");
+        System.out.println(place);
+    }
+
+    @Test
+    void groupAllIssues() {
+        List<Issue> place = gitLabService.groupAllIssues("4207231", 30, 1);
+        assertTrue(place.size() != 0, "the list of issues is empty");
+        System.out.println(place);
+    }
+
+    @Test
+    void groupIssueComments() {
+        List<Comment> place = gitLabService.groupIssueComments("4207231", "1234", 2);
+        assertTrue(place.size() != 0, "the list of comments is empty");
+        System.out.println(place);
+    }
+
+    // funcionGetgitLabMiner(String id, Integer since, Integer updated_after, Integer maxPages){ Project proyecto = getProjectById(id).getBody()
+    // ProjectGitMiner proyectoRefactor = formateaProyecto(proyecto)}
+
+
 }
